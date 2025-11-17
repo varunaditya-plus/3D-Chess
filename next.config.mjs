d177f040e80f81cdac451570b7145a8eb83148d8
varunaditya-plus <varunaditya.2023@gmail.com>
2025-11-17 22:03:18 +0100

    chore: update nextjs stuff

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  /* config options here */
+};
 
-module.exports = nextConfig
-
+export default nextConfig;
